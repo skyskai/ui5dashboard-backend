b089a4863f4fb06fdf472c80405e9133410dbe75
Yang Seung Chul <skyskai@naver.com>
2017-11-06 16:02:52 +0900

    width text

--- index.js
+++ index.js
@@ -567,7 +567,7 @@ like line  \nbreaks`) // Note the two spaces before '\n' required for a
    .addButton('This is a button', 'https://assistant.google.com/')
   //  .setImage('https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
   .setImage(imageUrl,
-     'Image alternate text','100%'))
+     'Image alternate text','200px'))
  .addSimpleResponse({ speech: 'This is another simple response',
    displayText: 'This is the another simple response 💁' });
 
